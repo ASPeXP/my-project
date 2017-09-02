11376b68c14f4817d6ef52a0920ec71ede5d1a00
Mac <mac@PrapathMBP.local>
2017-09-02 22:43:38 +0700

    Initial project stage 0:

---
+++ app/views.py
@@ -0,0 +1,21 @@
+
+from flask import render_template
+
+from app import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/education')
+def education():
+    return render_template('education.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/contact')
+def about():
+    return render_template('contact.html')
+
