4fb9a28a8e5ea1be8f56944d10a3d91d97323b5a
Mac <mac@PrapathMBP.local>
2017-09-03 19:49:24 +0700

    updated for templates, view and top 2 header

--- app/views.py
+++ app/views.py
@@ -3,19 +3,28 @@ from flask import render_template
 
 from app import app
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/education')
 def education():
     return render_template('education.html')
 
+
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
 
+
 @app.route('/contact')
 def about():
     return render_template('contact.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
